46facd1b916a9f3303fd4c4c4867266a5bb15e5a
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-13 00:30:49 +0900

    Update 73. Set Matrix Zeroes.py

--- blind75+grid75/Matrix/73. Set Matrix Zeroes.py
+++ blind75+grid75/Matrix/73. Set Matrix Zeroes.py
@@ -49,16 +49,15 @@ class Solution:
                         matrix[i][0] = 0
                     else:
                         rowzero = True
-        #先处理非第一行和第一列的元素，因为第一行和第一列元素需要作为判断依据（不能全置0先）
         for i in range(1, m):
             for j in range(1, n):
-                if matrix[0][j] == 0 or matrix[i][0] == 0:#在便利所有元素时，通过判断该行/列的第一个元素是不是0来决定要不要对其进行置零
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
                     matrix[i][j] = 0
-        #处理第一列
+        
         if matrix[0][0] == 0:
             for i in range(m):
                 matrix[i][0] = 0
-        #处理第一行
+        
         if rowzero:
             for j in range(n):
                 matrix[0][j] = 0
