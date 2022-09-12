eacc7bab2f789c14faa6301f79de8a8a56a0a1d0
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-13 00:43:05 +0900

    Update 15. 3Sum.py

--- blind75+grid75/Array/15. 3Sum.py
+++ blind75+grid75/Array/15. 3Sum.py
@@ -6,9 +6,9 @@ class Solution:
         res = []
         nums.sort()
         for i, val in enumerate(nums):
-            if val > 0:#若当前的数大于0，因为数组递增，所以另外两个也大于0，不可能存在和为0的情况
+            if val > 0:
                 break
-            if i > 0 and val == nums[i - 1]:#跳过重复出现的元素
+            if i > 0 and val == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) - 1
             while l < r:
@@ -18,7 +18,7 @@ class Solution:
                     l += 1
                 else:
                     res.append([val, nums[l], nums[r]])
-                    while l + 1 < r and nums[l + 1] == nums[l]:#跳过重复出现的元素,先处理左边或右边都可以，因为上面的if语句会调整参数
+                    while l + 1 < r and nums[l + 1] == nums[l]:
                         l += 1
                     l += 1
         return res
