9b8391b675b519924096dc9751c64bf2d822c0f1
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-13 00:32:37 +0900

    Update 300. Longest Increasing Subsequence.py

--- blind75+grid75/Dynamic Programming/300. Longest Increasing Subsequence.py
+++ blind75+grid75/Dynamic Programming/300. Longest Increasing Subsequence.py
@@ -5,7 +5,7 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1] * len(nums) 
         for i in range(len(nums) - 2, -1, -1):#bottom-up dp base case:dp[len(nums)-1]=1
-            for j in range(i + 1, len(nums)):#e.g [0,3,2,3]0<3 and 0<2，所以在通过不断更新dp[i]在nums[i+1:]里找最大dp
+            for j in range(i + 1, len(nums)):#e.g [0,3,2,3]0<3 and 0<2
                 if nums[i] < nums[j]:
                     dp[i] = max(dp[i], 1 + dp[j])
         return max(dp)
