6c00eaeb0eb6412579720bc05e674cfc655d639e
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-13 00:32:11 +0900

    Update 139. Word Break.py

--- blind75+grid75/Dynamic Programming/139. Word Break.py
+++ blind75+grid75/Dynamic Programming/139. Word Break.py
@@ -6,10 +6,10 @@ class Solution:
         dp = [False] * len(s) + [True]
         
         for i in range(len(s) - 1, -1, -1):
-            for word in wordDict:#下面第一个判断句加括号执行更快
-                if (i + len(word)) <= len(s) and s[i: i + len(word)] == word:#在保证当前下标开始读len(word)长度的字母时，没有超出s的最大下标的前提下，判断两个单词是否相同
+            for word in wordDict:
+                if (i + len(word)) <= len(s) and s[i: i + len(word)] == word:
                     dp[i] = dp[i + len(word)]
-                if dp[i]:#找到了，就跳出当前内循环
+                if dp[i]:
                     break
         return dp[0]
 
@@ -27,15 +27,15 @@ class Solution:
         visited.add(0)
         
         while q:
-            cur_index = q.popleft()#bfs也可以，visited的作用都是一样的
+            cur_index = q.popleft()
             for i in range(cur_index + 1, len(s) + 1):
-                if i in visited:#如果不用visited检测的话，已经被标记过的i可能又会被标记一次，太耗时(因为cur_index < 被标记的i，所以新的循环中，i可能会走到之前被标记的i的位置，为了避免再次执行下面的四句话，必须使用visited记录),eg.["aaaaaa"] ["aaa","aa"]
+                if i in visited:#edge case eg.["aaaaaa"] ["aaa","aa"]
                     continue
                 if s[cur_index : i] in wordDict:
                     if i == len(s):
                         return True
                     q.append(i)
-                    visited.add(i)#标记i表示i下标之前的subtring满足目标函数
+                    visited.add(i)
         return False
 
 # Time Complexity: O(n^2) (O(n^3)若考虑比较字符串的时间)
