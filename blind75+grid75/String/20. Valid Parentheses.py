229226f25bfd998f39391e025486edfd12c951ff
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-13 00:38:46 +0900

    Update 20. Valid Parentheses.py

--- blind75+grid75/String/20. Valid Parentheses.py
+++ blind75+grid75/String/20. Valid Parentheses.py
@@ -7,13 +7,13 @@ class Solution:
         hashmap = {')': '(', '}': '{', ']': '['}
         for c in s:#O(n)
             if c in hashmap:
-                if stack and stack[-1] == hashmap[c]:#stack不为空且括号匹配上了
+                if stack and stack[-1] == hashmap[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
-        return True if not stack else False #stack为空，返回true，因为题给条件是s.length>1，所以stack为空的情况只能是成功了
+        return True if not stack else False 
 #e.g stack=['(','('] we should return False
 
 #TC: O(n)
