24bc8336f07f0f84ef25c6e79190d10ebfcc45d0
EIEI <99803920+eiei7@users.noreply.github.com>
2022-09-18 18:11:27 +0900

    Update 76. Minimum Window Substring.py

--- blind75+grid75/String/76. Minimum Window Substring.py
+++ blind75+grid75/String/76. Minimum Window Substring.py
@@ -3,35 +3,35 @@ Method: sliding window
 """
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ""
-        total = 0
-        count = {}
-        for c in t: #O(m)
-            count[c] = 1 + count.get(c, 0)
-            total += 1
+        if len(s) < len(t):
+            return ""
+        
+        hashmap = {}
+        for c in t:
+            hashmap[c] = 1 + hashmap.get(c, 0)    
+        total = len(hashmap)
+
         res = [0, float('inf')]
-        l = 0
-        for r in range(len(s)):#O(n)
-            if s[r] in t:
-                if count[s[r]] > 0:
+        l, r = 0, 0
+        while r < len(s):
+            if s[r] in hashmap:
+                hashmap[s[r]] -= 1
+                if hashmap[s[r]] == 0:
                     total -= 1
-                count[s[r]] -= 1
+
             if total == 0:
-                while True:
-                    if s[l] not in t:#O(m + n)
-                        l += 1
-                    elif s[l] in t and count[s[l]] < 0:
-                        count[s[l]] += 1
-                        l += 1
-                    else:
-                        break
-                if r - l < res[1] - res[0]:
-                    res = [l, r]
-                count[s[l]] += 1
-                if count[s[l]] > 0:
+                while s[l] not in t or hashmap[s[l]] < 0:
+                    if s[l] in t:
+                        hashmap[s[l]] += 1
+                    l += 1
+                if hashmap[s[l]] == 0:
+                    if r - l < res[1] - res[0]:
+                        res = [l, r]
+                    hashmap[s[l]] += 1
                     total += 1
-                l += 1
-        return s[res[0]: res[1] + 1] if res[1] != float('inf') else ""
+                    l += 1
+            r += 1
+        return s[res[0]:res[1] + 1] if res[1] != float('inf') else ""
 
 #TC: O(m + n)
 #SC: O(m + n)
